c965859202f91a0fa55e44a73048f338db2c67ab
FEZ <fez.de.de@gmail.com>
2018-05-23 23:20:10 +0200

    Meeteora Button now in JS (for later use like addthis)

---
+++ js/meeteora-button.js
@@ -0,0 +1,4 @@
+document.write('<center>' +
+'<a href="map.html" class="btn btn-outline-primary" id="meeteora-map-btn">' +
+'<img height="30" src="img/meeteora-bubble.svg"/>Start Map' +
+'</a></center>');
