252bfa6e45baa3b8b1e13a351046e5385d38bb78
FEZ <fez.de.de@gmail.com>
2018-05-24 23:07:42 +0200

    Button Script now comes with own CSS (not bootstrap required)

--- js/meeteora-button.js
+++ js/meeteora-button.js
@@ -1,4 +1,5 @@
+document.write('<link href="css/meeteora-button.css" type="text/css" rel="stylesheet">  ');
 document.write('<center>' +
-'<a href="map.html" class="btn btn-outline-primary" id="meeteora-map-btn">' +
+'<a href="map.html" class="meeteora-button">' +
 '<img height="30" src="img/meeteora-bubble.svg"/>Start Map' +
 '</a></center>');
