c56bc6321edd60c932348142031cd23e877bec52
FEZ <fez.de.de@gmail.com>
2018-06-02 21:49:21 +0200

    finish holiday sprint commit -> may not be stable

---
+++ js/meeteora-ui.js
@@ -0,0 +1,52 @@
+function initUi() {
+    initUsername();
+    initMenu();
+}
+
+function initMenu() {
+    $('.ui.dropdown').dropdown({
+        transition: 'drop'
+    });
+}
+
+function initUsername() {
+    $(".username-button").click(function () {
+        $('#username-modal').modal('show');
+    });
+
+    // Init the modal
+    $('#username-modal').modal("setting", {
+        //detachable: false,
+        closable: false,
+        onDeny: function () {
+            console.log("Deny / Cancel");
+
+        },
+        onApprove: function () {
+            var formValid = $('#username-form').form('is valid');
+            if (!formValid) {
+                $("#username").focus();
+                return false;
+            }
+            //TODO save name to cookie
+        },
+        onShow: function () {
+            //TODO set name from Cookie
+            $("#username").val(new Date());
+        },
+    });
+
+    // Set validation rules for the form
+    $('#username-form')
+        .form({
+            on: 'blur',
+            inline: true,
+            fields: {
+
+                username: ['minLength[2]', 'maxLength[5]', 'empty']
+            }
+        });
+}
+
+
+initUi();
